ded5e97355ff7e8b4e0f2b4607dc7c3a25c76ff9
Ryan Saunders <contact.ryan.saunders@gmail.com>
2015-11-09 22:08:38 -0700

    parsing data and beginning to add svg components

--- implementation/js/graphvis.js
+++ implementation/js/graphvis.js
@@ -11,20 +11,90 @@ function GraphVis(_parentElement, _data, _metaData) {
     self.data = _data;
     self.metaData = _metaData;
     self.displayData = [];
-
+    self.set = "Age";
+    self.updateData();
     self.initializeVis();
 }
 
-
 /**
  * Called as initial setup of object.
- * @return {[type]} [description]
  */
 GraphVis.prototype.initializeVis = function () {
+    var xMax, xMin, yMax, yMin;
+    // var selectionX, selectionY;
+    // var xAxisG, yAxisG;
     var self = this;
 
-    self.svg = self.parentElement.select("svg");
+    self.svg = d3.select("#graphVis1");
 
     self.svgWidth = 340;
     self.svgHeight = 200;
-}
+
+    self.svgMarginLeft = 40;
+    self.svgMarginLeft = 40;
+    self.svgGraphWidth = self.svgWidth - self.svgMarginLeft;
+
+    // define the max/min of x and y
+    xMax = d3.max(self.displayData, function (d) {
+                return d.value;
+            });
+    xMin = d3.min(self.displayData, function (d) {
+                return d.value;
+            });
+    yMax = d3.max(self.displayData, function (d) {
+                return d.count;
+            });
+    yMin = d3.min(self.displayData, function (d) {
+                return d.count;
+            });
+
+    self.xScale = d3.scale.linear()
+        .domain([xMin, xMax])
+        .range([0, self.svgGraphWidth]);
+    self.yScale = d3.scale.linear()
+        .domain([yMin, yMax])
+        .range([0, self.svgHeight]);
+
+    self.xAxis = d3.svg.axis().scale(self.xScale);
+    self.yAxis = d3.svg.axis().scale(self.yScale).orient("left");
+
+    self.svg.select(".xAxis")
+        .call(self.xAxis)
+        .attr("transform", "translate(" + self.svgMarginLeft + ",0)");
+    self.svg.select(".yAxis")
+        .call(self.yAxis)
+        .attr("transform", "translate(" + self.svgMarginLeft + ",0)");
+};
+
+/**
+ * Updates the display data.
+ */
+GraphVis.prototype.updateData = function () {
+    var self = this;
+    var i, j,
+        objectKeys,
+        value,
+        tempData = {};
+
+    self.displayData = [];
+
+    for (i = 0; i < self.data.length; i++) {
+        if (!tempData[self.data[i][self.set]]) {
+            tempData[self.data[i][self.set]] = 0;
+        }
+        tempData[self.data[i][self.set]] += 1;
+    }
+
+    objectKeys = Object.keys(tempData);
+    for (i = 0; i < objectKeys.length; i++) {
+        value = objectKeys[i];
+        // some sets are continous
+        if (self.set === "Age") {
+            value = parseInt(value);
+        }
+        self.displayData.push({
+            value: value,
+            count: tempData[objectKeys[i]]
+        });
+    }
+};
--- implementation/js/main.js
+++ implementation/js/main.js
@@ -24,8 +24,122 @@ $(function(){
     }
 
     /**
+     * If the given index of allData[i][key] === null, then instead = 0
+     */
+    function fromNullToZero(i, key) {
+        if (!allData[i][key]) {
+            allData[i][key] = 0;
+        } 
+    }
+
+    /**
+     * If the given index of allData[i][key] === null | "Refused" | "Dont know", then instead = "No"
+     */
+    function fromNullToNo(i, key) {
+        if (!allData[i][key] ||
+        allData[i][key] === "Refused" ||
+        allData[i][key] === "Dont know") {
+            allData[i][key] = "No";
+        }
+    }
+
+    /**
+     * Corrects values in allData to be ready for visualization
+     */
+    function normalizeData() {
+        var i;
+
+        for (i=0; i < allData.length; i++) {
+            fromNullToZero(i, "# days used marijuana or hashish/month");
+            fromNullToZero(i, "# days used methamphetamine/month");
+            fromNullToZero(i, "# of days used cocaine/month");
+            fromNullToZero(i, "# of days used heroin/month");
+            fromNullToZero(i, "# sex partners five years older/year");
+            fromNullToZero(i, "# sex partners five years younger/year");
+            if (!allData[i]["# times had sex without condom/year"] ||
+            allData[i]["# times had sex without condom/year"] === "Dont know" ||
+            allData[i]["# times had sex without condom/year"] === "Refused") {
+                allData[i]["# times had sex without condom/year"] = "No sex";
+            }
+            if (!allData[i]["# times had vaginal or anal sex/year"] ||
+            allData[i]["# times had vaginal or anal sex/year"] === "Dont know" ||
+            allData[i]["# times had vaginal or anal sex/year"] === "Refused") {
+                allData[i]["# times had vaginal or anal sex/year"] = "Never";
+            }
+            if (allData[i]["Annual household income"] === "Dont know" ||
+            allData[i]["Annual household income"] === "Over $20,000" ||
+            allData[i]["Annual household income"] === "Under $20,000" ||
+            allData[i]["Annual household income"] === "Refused") {
+                allData[i]["Annual household income"] = null;
+            }
+            fromNullToZero(i, "Avg # alcoholic drinks/day -past 12 mos");
+            fromNullToZero(i, "Avg # cigarettes/day during past 30 days");
+            fromNullToZero(i, "Avg # alcoholic drinks/day -past 12 mos");
+            fromNullToZero(i, "Avg # alcoholic drinks/day -past 12 mos");
+            if (allData[i]["Dark green vegetables available at home"] === "Dont know" ||
+            !allData[i]["Dark green vegetables available at home"] ||
+            allData[i]["Dark green vegetables available at home"] === "Refused") {
+                allData[i]["Dark green vegetables available at home"] = "Sometimes";
+            }
+            fromNullToZero(i, "Day vigorous recreation / week");
+            fromNullToZero(i, "Days walk or bike / week");
+            if (allData[i]["Do you bike"] === "Dont know") {
+                allData[i]["Do you bike"] = "No";
+            }
+            if (!allData[i]["Do you now smoke cigarettes"]) {
+                allData[i]["Do you now smoke cigarettes"] = "Not at all";
+            }
+            if (!allData[i]["Education"] ||
+            allData[i]["Education"] === "Dont Know" ||
+            allData[i]["Education"] === "Refused") {
+                allData[i]["Education"] = "Less Than 9th Grade";
+            }
+            fromNullToNo(i, "Ever been in rehabilitation program");
+            fromNullToNo(i, "Ever have 5 or more drinks every day?");
+            fromNullToNo(i, "Ever use any form of cocaine");
+            fromNullToNo(i, "Ever used heroin");
+            fromNullToNo(i, "Ever used marijuana or hashish");
+            fromNullToNo(i, "Ever used methamphetamine");    
+            if (allData[i]["Fat-free/low fat milk available at home"] === "Dont know" ||
+            !allData[i]["Fat-free/low fat milk available at home"] ||
+            allData[i]["Fat-free/low fat milk available at home"] === "Refused") {
+                allData[i]["Fat-free/low fat milk available at home"] = "Sometimes";
+            } 
+            if (allData[i]["Fruits available at home"] === "Dont know" ||
+            !allData[i]["Fruits available at home"] ||
+            allData[i]["Fruits available at home"] === "Refused") {
+                allData[i]["Fruits available at home"] = "Sometimes";
+            } 
+            fromNullToNo(i, "Had at least 12 alcohol drinks/1 yr?"); 
+            fromNullToNo(i, "Had sex with new partner/year");
+            if (allData[i]["Marital status"] === "Dont know" ||
+            !allData[i]["Marital status"] ||
+            allData[i]["Marital status"] === "Refused") {
+                allData[i]["Marital status"] = "No Answer";
+            } 
+            fromNullToZero(i, "Money spent at supermarket/grocery store");
+            fromNullToZero(i, "Money spent on carryout/delivered foods");
+            fromNullToZero(i, "Money spent on eating out");
+            if (allData[i]["Salty snacks available at home"] === "Dont know" ||
+            !allData[i]["Salty snacks available at home"] ||
+            allData[i]["Salty snacks available at home"] === "Refused") {
+                allData[i]["Salty snacks available at home"] = "Sometimes";
+            }
+            fromNullToNo(i, "Smoked at least 100 cigarettes in life");    
+            if (allData[i]["Soft drinks available at home"] === "Dont know" ||
+            !allData[i]["Soft drinks available at home"] ||
+            allData[i]["Soft drinks available at home"] === "Refused") {
+                allData[i]["Soft drinks available at home"] = "Sometimes";
+            }
+            fromNullToZero(i, "Total number of sex partners/year");
+
+            // it's hard to decipher what NULL means...
+            delete allData[i]["Tried to quit smoking"];           
+        }
+    }
+    /**
      * FOR DEBUGGING:
-     * Parses the data to extract relevant information .
+     * Parses the data to extract relevant information.
      * @param  {object} data            the data to parse
      */
     function parseData(data, printToConsole) {
@@ -85,6 +199,7 @@ $(function(){
         allData = _allData;
         metaData = _metaData;
 
+        normalizeData();
         parseData(allData, true);
 
         initializeVisualizations();
